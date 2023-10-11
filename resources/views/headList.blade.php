e7330c0ff7e6d4e59aa519aabac7d4c448928d46
Shoma Takami <46513524+ShomaTakami@users.noreply.github.com>
2023-10-11 14:23:29 +0900

    aa

--- resources/views/headList.blade.php
+++ resources/views/headList.blade.php
@@ -8,6 +8,7 @@
 <script src="{{ asset('js/common.js') }}"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
+
     $(document).ready(function() {
               $(".js-nav-link").click(function(event) {
                   event.preventDefault(); // リンクのデフォルトの動作を無効化
