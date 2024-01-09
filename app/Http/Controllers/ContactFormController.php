9e0b9d820b33fd1c10800fc56b113ff3d153e659
ShomaTakami <hawklook.2331@gmail.com>
2024-01-09 11:35:08 +0900

    top

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -25,26 +25,32 @@ class ContactFormController extends Controller
             'recaptchaToken' => 'required', // 新しく追加
         ]);
 
-        // reCAPTCHA トークンの検証
-        $secret_key = "6LejpUopAAAAAO9S9HTovSILDArnzLgslVfFdALa";
-        if(isset($_POST['g-recaptcha-response'])){
-        $captcha=$_POST['g-recaptcha-response'];
-        }
-        if(!$captcha){
-        echo '<h2>reCAPTCHA にチェックを入れて下さい。</h2><br>';
-        echo "<a href=\"javascript:history.go(-1)\">Go Back</a>";
-        exit;
-        }
-        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=" . $secret_key . "&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
-        if ($response->success) {
-            // 認証成功時の処理
-            // メールを送信
-            Mail::to('fastline.llcompany@gmail.com')->send(new ContactFormMail($request->all()));
-            // メール送信後のリダイレクトなどの処理
-            return redirect()->route('top');
+        $recaptcha = $_POST['g-recaptcha-response'];
+        if (isset($recaptcha)) {
+            $captcha = $recaptcha;
         } else {
-            // 認証エラー時の処理
-            return redirect()->route('error');
+            $captcha = '';
         }
+        $secretKey = "6LejpUopAAAAAO9S9HTovSILDArnzLgslVfFdALa";
+        $url = "https://www.google.com/recaptcha/api/siteverify?secret={$secretKey}&response={$captcha}";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        $resp_result = json_decode($result, true);
+        if (intval($resp_result["success"]) !== 1) {
+            // reCAPTCHA承認失敗時の処理
+            header('Location: https://fastline-llc.com');
+            exit;
+        }
+
+        // 認証成功時の処理
+        // メールを送信
+        Mail::to('fastline.llcompany@gmail.com')->send(new ContactFormMail($request->all()));
+        // メール送信後のリダイレクトなどの処理
+        return redirect()->route('top');
     }
 }
