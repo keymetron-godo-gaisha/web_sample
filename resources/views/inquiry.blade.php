5a0f0c5ea0ab0a034ddef969f1caace56df57b13
ShomaTakami <hawklook.2331@gmail.com>
2024-01-09 10:21:55 +0900

    aa

--- resources/views/inquiry.blade.php
+++ resources/views/inquiry.blade.php
@@ -137,13 +137,7 @@
                             {{-- <input type="hidden" name="g-recaptcha-response" id="g-recaptcha-response"> --}}
                             {{-- <input type="hidden" name="recaptchaResponse" id="recaptchaResponse"> --}}
                             <input type="hidden" name="recaptchaToken" id="recaptchaToken">
-                            {{-- <input type="submit" class="send-submit-push" value="プライバシーポリシーに同意して送信する"> --}}
-                            <button class="g-recaptcha"
-    data-sitekey="6Le5hkopAAAAAGVvpKIplMkOFchmX7esZkVdmf2Q"
-    data-callback='onSubmit'
-    data-action='submit'>
-  Submit
-</button>
+                            <input type="submit" class="send-submit-push" value="プライバシーポリシーに同意して送信する">
                         </div>
                     </div>
                 </div>
