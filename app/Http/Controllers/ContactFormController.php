0fc89b11cf3759c5221d1e3014be5b5850a2ec80
ShomaTakami <hawklook.2331@gmail.com>
2024-01-09 10:37:02 +0900

    test

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -12,31 +12,32 @@ class ContactFormController extends Controller
 {
     public function sendInquiry(Request $request)
     {
-        if (isset($_POST["recaptchaResponse"]) && !empty($_POST["recaptchaResponse"])) {
+        if (isset($_POST["recaptchaToken"]) && !empty($_POST["recaptchaToken"])) {
             $secret = "6Le5hkopAAAAAGVvpKIplMkOFchmX7esZkVdmf2Q";
-            $verifyResponse = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secret."&response=".$_POST["recaptchaResponse"]);
+            $verifyResponse = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secret."&response=".$_POST["recaptchaToken"]);
             $reCAPTCHA = json_decode($verifyResponse);
             if ($reCAPTCHA->success) {
               echo "認証成功";
+              // フォームデータを受け取り、バリデーションを行う
+              $data = $request->validate([
+                  'lastname' => 'required',
+                  'firstname' => 'required',
+                  'company' => 'nullable',
+                  'department' => 'nullable',
+                  'email' => 'required|email',
+                  'phone' => 'nullable',
+                  'message' => 'nullable',
+              ]);
+                  // メールを送信
+                  Mail::to('fastline.llcompany@gmail.com')->send(new ContactFormMail($data));
+                  // メール送信後のリダイレクト
+                  return redirect()->route('top');
             } else {
               echo "認証エラー";
             }
         } else {
             echo "不正アクセス";
-        }
-        // フォームデータを受け取り、バリデーションを行う
-        $data = $request->validate([
-            'lastname' => 'required',
-            'firstname' => 'required',
-            'company' => 'nullable',
-            'department' => 'nullable',
-            'email' => 'required|email',
-            'phone' => 'nullable',
-            'message' => 'nullable',
-        ]);
-            // メールを送信
-            Mail::to('fastline.llcompany@gmail.com')->send(new ContactFormMail($data));
-            // メール送信後のリダイレクト
             return redirect()->route('top');
+        }
     }
 }
