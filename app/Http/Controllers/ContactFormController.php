8ade4df9bec1f4206bcbd1fb3d187abbcfba5b98
ShomaTakami <hawklook.2331@gmail.com>
2024-01-09 11:31:32 +0900

    ff

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -26,13 +26,17 @@ class ContactFormController extends Controller
         ]);
 
         // reCAPTCHA トークンの検証
-        $recaptchaToken = $request->input('recaptchaToken');
-        $recaptchaSecret = config('services.recaptcha.secret'); // config/services.php に設定しておくと良い
-
-        $verifyResponse = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$recaptchaSecret}&response={$recaptchaToken}");
-        $reCAPTCHA = json_decode($verifyResponse);
-dd($reCAPTCHA);
-        if ($reCAPTCHA->success) {
+        $secret_key = "6LejpUopAAAAAO9S9HTovSILDArnzLgslVfFdALa";
+        if(isset($_POST['g-recaptcha-response'])){
+        $captcha=$_POST['g-recaptcha-response'];
+        }
+        if(!$captcha){
+        echo '<h2>reCAPTCHA にチェックを入れて下さい。</h2><br>';
+        echo "<a href=\"javascript:history.go(-1)\">Go Back</a>";
+        exit;
+        }
+        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=" . $secret_key . "&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
+        if ($response->success) {
             // 認証成功時の処理
             // メールを送信
             Mail::to('fastline.llcompany@gmail.com')->send(new ContactFormMail($request->all()));
